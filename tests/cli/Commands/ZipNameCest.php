94b8284e42b44b91f100b7c610217369182d6a53
test <test@test.test>
2023-07-09 23:10:00 -0400

    Adding tests for pup zip-name

---
+++ tests/cli/Commands/ZipNameCest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace StellarWP\Pup\Tests\Cli\Commands;
+
+use StellarWP\Pup\Tests\Cli\AbstractBase;
+use StellarWP\Pup\Tests\CliTester;
+
+class ZipNameCest extends AbstractBase {
+
+	/**
+	 * @test
+	 */
+	public function it_should_get_the_zip_name_from_the_plugin( CliTester $I ) {
+		$this->write_default_puprc();
+
+		chdir( $this->tests_root . '/_data/fake-project' );
+
+		$I->runShellCommand( "php {$this->pup} zip-name" );
+		$I->seeResultCodeIs( 0 );
+		$I->seeInShellOutput( 'fake-project.1.0.0.1' );
+
+		$output = $I->grabShellOutput();
+		$this->assertMatchesStringSnapshot( $output );
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_should_get_the_dev_zip_name_from_the_plugin( CliTester $I ) {
+		$this->write_default_puprc();
+
+		chdir( $this->tests_root . '/_data/fake-project' );
+
+		$I->runShellCommand( "php {$this->pup} zip-name --dev" );
+		$I->seeResultCodeIs( 0 );
+		$I->seeInShellOutput( 'fake-project.1.0.0.1-dev' );
+
+		$output = $I->grabShellOutput();
+		$this->assertMatchesStringSnapshot( $output );
+	}
+}
