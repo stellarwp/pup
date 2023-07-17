b5fb20f58aed1786bde7a9578c1330ef7726a434
test <test@test.test>
2023-07-17 12:57:58 -0400

    Add more regex handling rules

--- src/Commands/Package.php
+++ src/Commands/Package.php
@@ -292,14 +292,59 @@ class Package extends Command {
 	public function globstarToRegex( string $original_pattern ): string {
 		$pattern = ltrim( $original_pattern, '/' );
 
-		// We don't want to quote the glob pattern `**/` in regex, later we'll replace it with an appropriate regex.
+		// Prevent escaping of desired patterns. Capture and adjust supported patterns.
 		$pattern = str_replace( '**/', '&glob;', $pattern );
-
-		// We don't want to quote `*` in regex pattern, later we'll replace it with `.*`.
+		$pattern = str_replace( ']*', ']&squareast;', $pattern );
+		$pattern = str_replace( '[:upper:]', '&posixupper;', $pattern );
+		$pattern = str_replace( '[:lower:]', '&posixlower;', $pattern );
+		$pattern = str_replace( '[:alpha:]', '&posixalpha;', $pattern );
+		$pattern = str_replace( '[:digit:]', '&posixdigit;', $pattern );
+		$pattern = str_replace( '[:xdigit:]', '&posixxdigit;', $pattern );
+		$pattern = str_replace( '[:alnum:]', '&posixalnum;', $pattern );
+		$pattern = str_replace( '[:punct:]', '&posixpunct;', $pattern );
+		$pattern = str_replace( '[:blank:]', '&posixblank;', $pattern );
+		$pattern = str_replace( '[:space:]', '&posixspace;', $pattern );
+		$pattern = str_replace( '[:word:]', '&posixword;', $pattern );
+		$pattern = (string) preg_replace( '/\+\(([^)\/]+)\)/', '($1)&pluscapture;', $pattern );
+		$pattern = (string) preg_replace( '/\*\(([^)\/]+)\)/', '($1)&astcapture;', $pattern );
+		$pattern = (string) preg_replace( '/\?\(([^)\/]+)\)/', '($1)&questcapture;', $pattern );
+		$pattern = (string) preg_replace( '/@\(([^)\/]+)\)/', '($1)&atcapture;', $pattern );
+		$pattern = str_replace( '?', '&question;', $pattern );
+		$pattern = str_replace( '(', '&openparen;', $pattern );
+		$pattern = str_replace( ')', '&closeparen;', $pattern );
+		$pattern = str_replace( '[', '&openbracket;', $pattern );
+		$pattern = str_replace( ']', '&closebracket;', $pattern );
+		$pattern = str_replace( '|', '&pipe;', $pattern );
+		$pattern = str_replace( '+', '&plus;', $pattern );
 		$pattern = str_replace( '*', '&ast;', $pattern );
+
+		// Escape the regex.
 		$pattern = preg_quote( $pattern, '/' );
+
+		// Convert placeholders back into supported patterns.
 		$pattern = str_replace( '&glob;', '(.+\/)?', $pattern );
+		$pattern = str_replace( '&question;', '?', $pattern );
+		$pattern = str_replace( '&openparen;', '(', $pattern );
+		$pattern = str_replace( '&closeparen;', ')', $pattern );
+		$pattern = str_replace( '&openbracket;', '[', $pattern );
+		$pattern = str_replace( '&closebracket;', ']', $pattern );
+		$pattern = str_replace( '&pipe;', '|', $pattern );
+		$pattern = str_replace( '&plus;', '+', $pattern );
+		$pattern = str_replace( '&pluscapture;', '+', $pattern );
+		$pattern = str_replace( '&astcapture;', '*', $pattern );
+		$pattern = str_replace( '&questcapture;', '?', $pattern );
+		$pattern = str_replace( '&atcapture;', '{1}', $pattern );
 		$pattern = str_replace( '&ast;', '[^\/]*', $pattern );
+		$pattern = str_replace( '&posixupper;', '[A-Z]', $pattern );
+		$pattern = str_replace( '&posixlower;', '[a-z]', $pattern );
+		$pattern = str_replace( '&posixalpha;', '[a-zA-Z]', $pattern );
+		$pattern = str_replace( '&posixdigit;', '[\d]', $pattern );
+		$pattern = str_replace( '&posixxdigit;', '[\dA-Fa-f]', $pattern );
+		$pattern = str_replace( '&posixalnum;', '[a-zA-Z\d]', $pattern );
+		$pattern = str_replace( '&posixblank;', '[ \t]', $pattern );
+		$pattern = str_replace( '&posixspace;', '[\s]', $pattern );
+		$pattern = str_replace( '&posixword;', '[\w]', $pattern );
+		$pattern = str_replace( '&squareast;', '*', $pattern );
 
 		// If the entry is tied to the beginning of the path, add the `^` regex symbol.
 		if ( 0 === strpos( $original_pattern, '/' ) ) {
@@ -371,6 +416,33 @@ class Package extends Command {
 	}
 
 	/**
+	 * Get the files to include in sync.
+	 *
+	 * @param array<int, string> $include
+	 * @param array<int, string> $ignore
+	 *
+	 * @return array<string, array<int, string>>
+	 */
+	public function migrateNegatedIncludeLinesToIgnore( array $include, array $ignore ): array {
+		$final_include = [];
+		$final_ignore  = $ignore;
+
+		foreach ( $include as $line ) {
+			if ( strpos( $line, '!' ) === 0 ) {
+				$final_ignore[] = substr( $line, 1 );
+				continue;
+			}
+
+			$final_include[] = $line;
+		}
+
+		return [
+			'include' => $final_include,
+			'ignore'  => $final_ignore,
+		];
+	}
+
+	/**
 	 * Sync files from one directory to another.
 	 *
 	 * This mimics rsync so that it works on non-unix systems.
@@ -386,8 +458,11 @@ class Package extends Command {
 
 		$this->buildSyncFiles( $source );
 
-		$ignore  = $this->getIgnoreLines( $source );
 		$include = $this->getIncludeLines( $source );
+		$ignore  = $this->getIgnoreLines( $source );
+		$results = $this->migrateNegatedIncludeLinesToIgnore( $include, $ignore );
+		$include = $results['include'];
+		$ignore  = $results['ignore'];
 
 		$iterator = new RecursiveIteratorIterator(
 			new RecursiveDirectoryIterator( $source, RecursiveDirectoryIterator::SKIP_DOTS ),
